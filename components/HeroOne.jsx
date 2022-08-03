0911ea0f2e3e5850425ae9c42ca6161c3f19299d
J. Graham <jgraham@Js-MacBook-Air.local>
2022-08-03 16:06:15 -0600

    added text to Hero

--- components/HeroOne.jsx
+++ components/HeroOne.jsx
@@ -2,8 +2,12 @@ import React from 'react'
 
 function HeroOne() {
   return (
-    <div className="w-screen h-[140vh]">
+    <div className="w-screen h-[140vh] relative">
       <img className="h-full w-full" src="/img/hero-one.jpg" alt="" />
+
+      <div className='w-full h-[20rem] flex items-center justify-center absolute top-[15rem] left-0'>
+        <h1 className='text-[2.5rem] font-serif text-black'>Lorem ipsum dolor sit</h1>
+      </div>
 
       <div className="w-screen h-40 flex items-center justify-center">
         <button className="absolute top-[52rem] w-52 h-12 text-white border text-[11px] font-extralight">READ MORE &rarr;</button>
